7b5fa38018d9a3ec524ffe40c8eb98bc53de8952
Jalen Henson <jhenson1@terpmail.umd.edu>
2025-12-09 13:49:59 -0500

    updated Welcome Screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function WelcomeScreen({ onContinue }) {
     return (
         <div className="Welcome-container" onClick={onContinue}>
-            <div className="Welcome-text">Hello!</div>
+            <div className="Welcome-text">Hello!\n</div>
+            <br></br>
             <div className="Welcome-subtext">Tap to continue</div>
         </div>
     );

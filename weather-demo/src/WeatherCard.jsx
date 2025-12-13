ed4e37080a194b742343dc51e9fc63c5d4076ca0
Reza Amanahu <70492029+rezaamanahu@users.noreply.github.com>
2025-12-13 16:42:07 -0500

    updating weather dates

--- weather-demo/src/WeatherCard.jsx
+++ weather-demo/src/WeatherCard.jsx
@@ -1,7 +1,13 @@
-export default function WeatherCard({ day, temp, icon, onClick }) {
+export default function WeatherCard({ day, temp,icon, onClick }) {
+    const dateFormat = new Date(day).toLocaleDateString("en-US", {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }); 
+
     return (
         <div className="weather-card" onClick={onClick}>
-            <h3>{day}</h3>
+            <h3>{dateFormat}</h3>
             <img src={`https:${icon}`}
                 alt="weather icon"
                 style={{width:"50px", height: "50px"}}

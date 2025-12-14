4b03be29be95a8c941f3197debeb8ddc716cadb1
jdeng123 <jdeng123@terpmail.umd.edu>
2025-12-14 03:03:58 -0500

    weather router get and simple server

--- weather-demo/server/server.js
+++ weather-demo/server/server.js
@@ -1,1 +1,40 @@
-/* backend code can go here */
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+const weatherRouter = require('./routes/weather.js');
+app.use('/api/weather', weatherRouter);
+
+const formRouter = require('./routes/formRouter.js'); 
+app.use('/api/form', formRouter);
+
+
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGO_CONNECTION_STRING;
+
+
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('MongoDB good ！'))
+  .catch(err => console.error('MongoDB failed：', err));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('/api/test', (req, res) => {
+  res.json({ msg: 'Express start！' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
--- weather-demo/src/api/weather.js
+++ weather-demo/src/api/weather.js
@@ -1,53 +1,20 @@
 import axios from 'axios';
 
-// ----- Jinye ---help me check it ! ----------
-
-const BASE_URL = process.env.REACT_APP_WEATHER_API_BASE_URL;
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-
 export const fetchWeather = async (location) => {
   try {
-    const response = await axios.get(`${BASE_URL}/forecast.json`, {
-      params: {
-        key: API_KEY,
-        q: location,
-        days: 7
-      }
-    });
-
-    const country = response.data.location.country; 
-
-    const weeklyData = response.data.forecast.forecastday.map((day) => ({
-      day: day.date,
-      temp_c: day.day.avgtemp_c,
-      temp_f: day.day.avgtemp_f,
-      icon: day.day.condition.icon,
-
-    }));
-
-
-    const hourlyData = response.data.forecast.forecastday[0].hour.slice(0, 12).map((hour) => ({
-      time: hour.time.split(' ')[1],
-      temp: hour.temp_c,
-      icon: hour.condition.icon,
-      condition: hour.condition.text
-    }));
-
-
+    const response = await axios.get(`/api/weather/${location}`);
+    const data = response.data;
     return {
-      country: country, 
-      weekly: weeklyData,
-      hourly: hourlyData
+      country: data.country,
+      weekly: data.weekly,
+      hourly: data.hourly
     };
-
   } catch (error) {
-    console.error('Error:', error);
-
+    console.error('Weather: ', error);
     return {
+      country: '',
       weekly: [],
       hourly: []
     };
   }
 };
-
-//------------------------------------
